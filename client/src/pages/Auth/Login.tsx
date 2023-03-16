f09e9ec41fb5ee098ee21853e78c2b59fea4d33c
catalinfl <catalinfl02@gmail.com>
2023-03-16 22:33:53 +0200

    login register form

--- client/src/pages/Auth/Login.tsx
+++ client/src/pages/Auth/Login.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { ReactHTML, useState } from 'react'
 import "./Login.scss"
 
+type LoginType = {
+    username: string,
+    password: string
+}
+
 const Login: React.FC = () => {
+
+    const [login, setLogin] = useState<LoginType>({
+        username: "",
+        password: ""
+    })
+
+    const loginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        axios.post("http://localhost:3001/auth/login")
+    }
+
+
+    const settingForm = (e: React.ChangeEvent<HTMLInputElement>, FormData: keyof LoginType) => {
+        setLogin({ ...login, [FormData]: e.target.value})
+    }
+
+
   return (
     <div className="login"> 
         <div className="loginContainer">
@@ -11,13 +34,13 @@ const Login: React.FC = () => {
 
                 </h3>
             </div>
-            <form className="loginForm">
-                <input spellCheck="false" className="loginInput" placeholder="Nickname" type="text" />
-                <input spellCheck="false" className="loginInput" placeholder="Password" type="password" />
+            <form className="loginForm" onSubmit={(e: React.FormEvent<HTMLFormElement>) => loginSubmit(e)}>
+                <input spellCheck="false" className="loginInput" placeholder="Nickname" type="text" onChange={(e: React.ChangeEvent<HTMLInputElement>) => settingForm(e, 'username')}/>
+                <input spellCheck="false" className="loginInput" placeholder="Password" type="password" onChange={(e: React.ChangeEvent<HTMLInputElement>) => settingForm(e, 'password')} />
+                <button className="loginButton">
+                    Login
+                </button>
             </form>
-            <button className="loginButton">
-                Login
-            </button>
         </div>
     </div>
   )
--- client/src/pages/Auth/Register.tsx
+++ client/src/pages/Auth/Register.tsx
@@ -1,25 +1,54 @@
-import React, { ReactElement } from 'react'
+import axios from 'axios'
+import React, { ReactElement, ReactHTML, useState } from 'react'
 import "./register.scss"
 
 
+type RegisterType = {
+    username: string,
+    email: string,
+    password: string
+}
+
 const Register: React.FC = (): ReactElement => {
+    
+    const initialObject: RegisterType = {
+        username: "",
+        email: "",
+        password: ""    
+    }
+    
+    const [register, setRegister] = useState<RegisterType>(initialObject)
+    
+    console.log(register)
+
+    
+    const registerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        axios.post("http://localhost:3001/auth/register")
+    }
+
+
+    const settingForm = (e: React.ChangeEvent<HTMLInputElement>, FormType: keyof RegisterType) => {
+        setRegister({ ...register, [FormType]: e.target.value})
+    }
+
     return (
       <div className="register"> 
           <div className="registerContainer">
               <div className="registerTitle">
                   <h3> Register now
                       &#9996;
-  
+
                   </h3>
               </div>
-              <form className="registerForm">
-                  <input spellCheck="false" className="registerInput" placeholder="Nickname" type="text" id="name" />
-                  <input spellCheck="false" className="registerInput" placeholder="E-mail" type="email" id="email" />
-                  <input spellCheck="false" className="registerInput" placeholder="Password" type="password" id="password" />
+              <form className="registerForm" onSubmit={(e: React.FormEvent<HTMLFormElement>) => registerSubmit(e)}>
+                  <input spellCheck="false" className="registerInput" placeholder="Nickname" type="text" id="name" onChange={(e: React.ChangeEvent<HTMLInputElement>) => settingForm(e, "username") } />
+                  <input spellCheck="false" className="registerInput" placeholder="E-mail" type="email" id="email" onChange={(e: React.ChangeEvent<HTMLInputElement>) => settingForm(e, "email") } />
+                  <input spellCheck="false" className="registerInput" placeholder="Password" type="password" id="password" onChange={(e: React.ChangeEvent<HTMLInputElement>) => settingForm(e, "password") } />
+                <button className="registerButton">
+                    Register
+                </button>
               </form>
-              <button className="registerButton">
-                  Register
-              </button>
           </div>
       </div>
     )
